e374db748bcdc75681a52f5854c6b47a75411ab3
Ant68s <140552023+Ant68s@users.noreply.github.com>
2024-11-07 16:05:21 +0100

    montecarlosim.C

--- MC/montecarlosim.C
+++ MC/montecarlosim.C
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+#include "TRandom3.h"
+#include "TH1D.h"
+
+const int Nchannels = 2048;
+const double min_edge = 0., max_edge = 2048.;
+
+TH1D* generator(int B=2){
+	TRandom3 *rg = new TRandom3(static_cast<int>(time(0))); // I MADE ONLY THIS CHANGE
+	float channel;
+	// Bin 0 - overflow; Bin Nchannels+1 - overflow; Bins 1 and Nchannels are bounds
+	TH1D* baseline_hist = new TH1D("Base", "Baseline", Nchannels, min_edge, max_edge);
+	
+	for (int i = 0; i < B*Nchannels; i ++){
+		channel = (int)std::floor((rg->Uniform(min_edge, max_edge)))+0.5;
+		baseline_hist->Fill(channel);
+	}
+	
+	baseline_hist->Draw();
+	std::cout << baseline_hist->GetBinContent(0) << ' ' << baseline_hist->GetBinContent(1) << ' ' 
+		<< baseline_hist->GetBinContent(Nchannels) << ' ' << baseline_hist->GetBinContent(Nchannels+1) << std::endl;
+	return baseline_hist;
+}
+
+
 void montecarlosim(){
 
 TH1D* baseline_sum = new TH1D("Base", "Baseline average", 2048,0.,2048.);
